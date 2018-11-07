f5967a70ac4e252482782214e9b39c76d26a6613
ekramulmostafa <ekramul@bongobd.com>
2018-11-07 17:38:59 +0600

    Test Methods add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,30 +13,9 @@ class Home extends React.Component{
     }
 
     async getToken(){
-        /*let objectInstance = {
-            url: this.state.baseUrl + '/api/login_check',
-            method: 'post',
-            // headers: {'X-Custom-Header': 'foobar'},
-            data: {
-                "authentication_type" 	: "anonymous",
-                "anonymous_id"			: "bioscope_anonymous",
-                "client_type"     		: "web",
-                "client_id"       		: "web_"+Math.random(),
-                "channel"  				: "bioscope_web_anonymous"
-            }
-        };
-
-        await axios(objectInstance).then((res) => {
-
-            this.setState({token: res.data.token}) ;
-            localStorage.setItem('tokenSaved', this.state.token);
-            // localStorage.setItem('tokenSaved',)
-        }).catch(function (error) {
-            console.log(error)
-        });*/
 
         const data = await tokenGenerate(this.state.baseUrl);
-        // console.log(data);
+
         if(typeof data.token !== 'undefined'){
             this.setState({token: data.token}) ;
             localStorage.setItem('tokenSaved', this.state.token);
@@ -46,34 +25,8 @@ class Home extends React.Component{
     }
 
     async getContntList(){
-        /*let objectInstance = {
-            // url: this.baseURL + '/api/en/content-selector/feluda-full-series',
-            url: this.state.baseUrl + '/api/en/content-selector/drama-dhamaka',
-            method: 'get',
-            headers: {
-                "Content-Type" : "application/json",
-                "Authorization" : "Bearer " + this.state.token,
-                "Access-Code" : "QkQ%3D"
-            },
-        };
-
-        await axios(objectInstance).then((res) => {
-
-            console.log(res.data._embedded.contents);
-            this.setState({contentLists : res.data._embedded.contents});
-            this.getData();
-
-
-        }).catch((error) => {
-            console.log('error'+error);
-            localStorage.setItem('tokenSaved', '');
-            this.getToken();
-        });*/
-
-
         const data = await contentList(this.state.baseUrl, this.state.token)
 
-        // console.log(JSON.stringify(data, null, 2));
         if(typeof data.contentLists !== 'undefined'){
             this.setState({contentLists : data.contentLists});
             this.getData();
@@ -100,7 +53,6 @@ class Home extends React.Component{
     }
 
     render(){
-        const {token, contentList} = this.state
         return(
             <div className="total-content-list">
                 <h1>Content list(React JS)</h1>
